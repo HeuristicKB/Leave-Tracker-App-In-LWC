bc8f9aebcaf6a4d6a00aea35c7b9e1110cc57db0
Khushal Bendkoli <bendkolikhushal10@gmail.com>
2023-08-18 13:49:03 +0530

    Delete LeaveTracker directory

--- LeaveTracker/force-app/main/default/lwc/leaveTracker/leaveTracker.js
+++
@@ -1,8 +0,0 @@
-import { LightningElement } from 'lwc';
-
-export default class LeaveTracker extends LightningElement {
-
-    refreshLeaveRequestsHandler(){
-        this.refs.myLeavesComp.refreshGrid();
-    }
-}
