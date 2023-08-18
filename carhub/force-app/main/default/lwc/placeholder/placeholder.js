8b75239b21fba350af720e77e99648c640ac3018
Khushal Bendkoli <bendkolikhushal10@gmail.com>
2023-08-18 13:17:49 +0530

    Delete carhub directory

--- carhub/force-app/main/default/lwc/placeholder/placeholder.js
+++
@@ -1,7 +0,0 @@
-import { LightningElement, api } from 'lwc';
-// Static resource
-import CAR_HUB_PLACEHOLDER from '@salesforce/resourceUrl/placeholder'
-export default class Placeholder extends LightningElement {
-    @api message
-    placeholderUrl = CAR_HUB_PLACEHOLDER
-}
